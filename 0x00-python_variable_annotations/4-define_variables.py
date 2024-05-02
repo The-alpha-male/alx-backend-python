8fdbb913bc098cab0c5c75be480302789367c5b8
The-alpha-male <ronnymunene@outlook.com>
2024-05-03 02:35:43 +0300

    test3

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""defines variables with specified values and annotations"""
+"""Contains definition of variables with specified values and annotations."""
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = “Holberton”
+school: str = "Holberton"
 
 # #!/usr/bin/env python3
-# """Contains definition of variables with specified values and annotations."""
+# """defines variables with specified values and annotations"""
 
 # a: int = 1
 # pi: float = 3.14
 # i_understand_annotations: bool = True
-# school: str = "Holberton"
+# school: str = “Holberton”
