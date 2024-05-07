be0a0def4e414d7daa60f0d0ba0662ff66d28bbf
The-alpha-male <ronnymunene@outlook.com>
2024-05-07 04:03:13 +0300

    made the files executable

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-"""Wait N module"""
+"""Let's execute multiple coroutines at
+the same time with async
+"""
 import asyncio
+import random
 from typing import List
-
-wait_random = __import__("0-basic_async_syntax").wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Returns list of all delays"""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
-    await_times = await asyncio.gather(*tasks, return_exceptions=False)
-    return sorted(await_times)
+    """Execute wait_random n times with the specified max_delay.
+    Args:
+        @n: int
+        @max_delay: int
+    return:
+        @List[float]
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    completed_delays = [await task for task in asyncio.as_completed(tasks)]
+    return completed_delays
