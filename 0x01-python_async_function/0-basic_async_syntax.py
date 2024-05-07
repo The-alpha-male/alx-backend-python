91d7994d40b900d807426149ddea7c0263204d58
The-alpha-male <ronnymunene@outlook.com>
2024-05-07 04:00:43 +0300

    basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-"""Concurrent coroutines"""
-
+"""The basics of async
+"""
 import asyncio
-from typing import List
-
-wait_random = __import__('0-basic_async_syntax').wait_random
+import random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Return the list of all delays"""
-    delays = []
-    for _ in range(n):
-        delays.append(wait_random(max_delay))
-    return [await delay for delay in asyncio.as_completed(delays)]
+async def wait_random(max_delay: int = 10) -> float:
+    """asynchronous coroutine
+    param:
+        @max_delay: int
+    return:
+        float
+    """
+    rand_num = random.uniform(0, max_delay)
+
+    await asyncio.sleep(rand_num)
+
+    return rand_num
