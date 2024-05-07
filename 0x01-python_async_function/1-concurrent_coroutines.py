a81110e19259a7f23907cdb47b52d10a1497027d
The-alpha-male <ronnymunene@outlook.com>
2024-05-07 03:49:13 +0300

    Asynchronous coroutine that takes in the integer argument

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-"""Concurrent coroutines"""
-
+"""Wait N module"""
 import asyncio
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Return the list of all delays"""
-    delays = []
-    for _ in range(n):
-        delays.append(wait_random(max_delay))
-    return [await delay for delay in asyncio.as_completed(delays)]
+    """Returns list of all delays"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    await_times = await asyncio.gather(*tasks, return_exceptions=False)
+    return sorted(await_times)
